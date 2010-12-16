9d230cee13c161adde886c1b21e0e0800bfc8fce
Konstantin Sering <konstantin.sering@gmail.com>
2010-12-16 12:29:56 +0100

    improved check.R

--- check.R
+++ check.R
@@ -5,18 +5,33 @@
 # (c) 2010 Konstantin Sering <konstantin.sering [aet] gmail.com>
 # GPL 3.0+ or (cc) by-sa (http://creativecommons.org/licenses/by-sa/3.0/)
 #
-# last mod 2010-12-02, KS
+# last mod 2010-12-09, KS
 
 
-setwd("C:\\Dokumente und Einstellungen\\user1\\Desktop\\visionlab_neu_20101122")
+setwd("C:/Dokumente und Einstellungen/user1/Desktop/visionlabutils/visionlab/data/")
 
-dat <- read.csv("color_table_20101123_2024.csv")
+dat <- read.csv("color_table_20101209_1220.csv", na.strings="NA", colClasses=c("character", rep("numeric", 16)))
+dat2 <- dat[171:180,]
+datTuned <- read.csv("color_table_20101210_1402.csv", na.strings="NA", colClasses=c("character", rep("numeric", 16)))
+datTuned2 <- datTuned[171:180,]
+
+# Y uebersicht
+with(dat, plot(monitor_xyY_Y))
+with(dat, points(tubes_xyY_Y, col="blue"))
+with(datTuned, points(tubes_xyY_Y, col="red"))
+X11()
+# x vs y uebersicht
+with(dat, plot(monitor_xyY_x, monitor_xyY_y, xlim=c(0.20,0.40), ylim=c(0.20,0.40)))
+with(dat, points(tubes_xyY_x, tubes_xyY_y, col="blue"))
+
 
 # Y
-plot(dat$monitor_xyY_Y)
-points(dat$tubes_xyY_Y, col="blue")
-
+with(dat2, plot(monitor_xyY_Y, ylim=c(16,26)))
+with(dat2, points(tubes_xyY_Y, col="blue"))
+with(datTuned2, points(tubes_xyY_Y, col="red"))
+X11()
 # x vs y
-plot(dat$tubes_xyY_x, dat$tubes_xyY_y, col="blue")
-points(dat$monitor_xyY_x, dat$monitor_xyY_y)
+with(dat2, plot(monitor_xyY_x, monitor_xyY_y, xlim=c(0.29,0.31), ylim=c(0.31,0.33)))
+with(dat2, points(tubes_xyY_x, tubes_xyY_y, col="blue"))
+with(datTuned2, points(tubes_xyY_x, tubes_xyY_y, col="red"))
 
