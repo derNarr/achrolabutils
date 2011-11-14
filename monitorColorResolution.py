e4ca697536a38a37579db2e7fbfa081db89b0369
Nora Umbach <nora.umbach@web.de>
2011-11-14 15:32:03 +0100

    added monitorColorResolution.py to measure 10bit

---
+++ monitorColorResolution.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# ./achrolabutils/calibrationMonitor.py
+#
+# (c) 2010 Konstantin Sering <konstantin.sering [aet] gmail.com> und Nora
+# Umbach <nora.umbach@web.de>
+# GPL 3.0+ or (cc) by-sa (http://creativecommons.org/licenses/by-sa/3.0/)
+#
+# last mod 2011-01-25, NU
+
+from achrolab.eyeone.EyeOne import EyeOne
+from achrolab.eyeone.EyeOneConstants import  (I1_MEASUREMENT_MODE, 
+                                    I1_SINGLE_EMISSION,
+                                    eNoError,
+                                    COLOR_SPACE_KEY, 
+                                    COLOR_SPACE_CIExyY,
+                                    SPECTRUM_SIZE,
+                                    TRISTIMULUS_SIZE)
+
+from psychopy import visual, core
+#import rpy2.robjects as robjects
+import time,pickle
+from ctypes import c_float
+
+# want to run R-commands with R("command")
+#R = robjects.r
+eye_one = EyeOne() #dummy=True)
+
+def getResolution(colorlist, imi=0.5, screen=0, colorSpace='rgb'):
+        """get the resolution of monitor with colors in colorlist.
+        EyeOne Pro should be connected to the computer. 
+        * colorlist -- a list of PatchStim values
+        * imi -- inter measurement interval."""
+
+        # set EyeOne Pro variables
+        if(eye_one.I1_SetOption(I1_MEASUREMENT_MODE, I1_SINGLE_EMISSION) ==
+                eNoError):
+            print("Measurement mode set to single emission.")
+        else:
+            print("Failed to set measurement mode.")
+            return
+        
+        if(eye_one.I1_SetOption(COLOR_SPACE_KEY, COLOR_SPACE_CIExyY) ==
+                eNoError):
+            print("Color space set to CIExyY.")
+        else:
+            print("Failed to set color space.")
+            return
+
+        # calibrate EyeOne Pro
+        print("\nPlease put EyeOne-Pro on calibration plate and "
+        + "press key to start calibration.")
+        while(eye_one.I1_KeyPressed() != eNoError):
+            time.sleep(0.01)
+        if (eye_one.I1_Calibrate() == eNoError):
+            print("Calibration of EyeOne Pro done.")
+        else:
+            print("Calibration of EyeOne Pro failed. Please RESTART.")
+            return
+        
+        ## measurement
+        win = visual.Window(size=(2048,1536), color=0.4, monitor='mymon',
+                screen=screen, colorSpace=colorSpace)
+
+        sizeSur = 8
+        #pos = sizeSur/2 + 0.02
+        patch_stim = visual.PatchStim(win, units='deg', size=sizeSur,
+                pos=[0,0], sf=0, color=-0.1450980, colorSpace=colorSpace)
+        patch_stim.setColor(-0.5, colorSpace=colorSpace)
+        patch_stim.draw()
+        win.flip()
+        
+        # prompt for click on button of EyeOne Pro
+        print("\nPlease put EyeOne-Pro in measurement position and hit"
+        + " button to start measurement.")
+        while(eye_one.I1_KeyPressed() != eNoError):
+            time.sleep(0.01)
+
+        print("Starting measurement...")
+        
+        xyY_list = list() # saves the measured xyY value
+
+        tristim = (c_float * TRISTIMULUS_SIZE)() # memory to where EyeOne
+                                                 # Pro saves TriStim.
+
+        for color in colorlist:
+            patch_stim.setColor(color, colorSpace=colorSpace)
+            print(color)
+            patch_stim.draw()
+            win.flip()
+
+            time.sleep(imi) # to give EyeOne Pro time to adapt and to
+                            # reduce carry-over effects
+
+            if(eye_one.I1_TriggerMeasurement() != eNoError):
+                print("Measurement failed for color %s ." %str(color))
+            if(eye_one.I1_GetTriStimulus(tristim, 0) != eNoError):
+                print("Failed to get TriStimulus for color %s ."
+                        %str(color))
+            xyY_list.append(list(tristim))
+
+        print("Measurement finished.")
+        calibfile = open('achrolab/calibdata/measurements/resolution_monitor' +
+                time.strftime("%Y%m%d_%H%M") + '.txt', 'w')
+        calibfile.write('\n'.join([str(x) for x in xyY_list]))
+
+if(__name__=="__main__"):
+
+    #patch_stim_value_list = [x/127.5 - 1 for x in range(120,130)]
+    patch_stim_rgb = [x for x in range(120,125)]
+
+    mywin = visual.Window(size=(2048,1536), monitor='mymon',
+                color=(1,1,1), screen=1, colorSpace='rgb')
+
+    getResolution(patch_stim_rgb, imi=0.5, screen=1, colorSpace='rgb255')
+    
