19aa323766ab0ebe96b662a7d2f8c0ed8d9f832d
Nora Umbach <nora.umbach@web.de>
2012-02-10 14:29:38 +0100

    worked on plots, took calibdata out of .gitignore (should be put back in there later)

--- tubes_spectra_plot.R
+++ tubes_spectra_plot.R
@@ -20,18 +20,23 @@
 #     }
 # dat2 <- as.data.frame(dat2)
 
-dat2 <- read.table("achrolabutils/calibdata/measurements/calibration_tubes_raw_20120208_1833.txt", sep=",", skip=1)
+dat1 <- read.table("achrolabutils/calibdata/measurements/calibration_tubes_raw_20120208_1833.txt", sep=",", skip=1)
 
+names(dat1) <- c("vR","vG","vB","x","y","Y",paste("l", 1:36, sep=""))
+
+# five measurements per step
+dat2 <- aggregate(as.matrix(dat1[,4:ncol(dat1)]) ~ vR + vG + vB, dat1, mean)
+
+dat2 <- dat2[order(dat2$vR, dat2$vG, dat2$vB),]
 names(dat2) <- c("vR","vG","vB","x","y","Y",paste("l", 1:36, sep=""))
 
 ###### (1) spectra for tubes ######
 
-plot(as.numeric(dat[1,7:42]), type="l")
 ## three separat plots
 par(mfrow=c(1,3))
 # red
 startp <- 0
-endp <- 500
+endp <- 100
 lcolor <- colorRampPalette(c("black","red"))(endp)
 plot(as.numeric(dat2[1,7:42]) ~ I(1:36), type="l", col=lcolor[startp],
     ylim=c(0,3))
@@ -40,23 +45,23 @@ for (i in startp:endp){
 }
 
 # green
-startp <- 501
-endp <- 1000
-lcolor <- colorRampPalette(c("black","green"))(500)
+startp <- 101
+endp <- 200
+lcolor <- colorRampPalette(c("black","green"))(100)
 plot(as.numeric(dat2[501,7:42]) ~ I(1:36), type="l", col=lcolor[1],
     ylim=c(0,3))
 for (i in startp:endp){
-    points(as.numeric(dat2[i,7:42]) ~ I(1:36), type="l", col=lcolor[i-500])
+    points(as.numeric(dat2[i,7:42]) ~ I(1:36), type="l", col=lcolor[i-100])
 }
 
 # blue
-startp <- 1001
-endp <- 1505
-lcolor <- colorRampPalette(c("black","blue"))(500)
+startp <- 201
+endp <- 300
+lcolor <- colorRampPalette(c("black","blue"))(100)
 plot(as.numeric(dat2[1001,7:42]) ~ I(1:36), type="l", col=lcolor[1],
     ylim=c(0,3))
 for (i in startp:endp){
-    points(as.numeric(dat2[i,7:42]) ~ I(1:36), type="l", col=lcolor[i-1000])
+    points(as.numeric(dat2[i,7:42]) ~ I(1:36), type="l", col=lcolor[i-200])
 }
 
 ## together in one plot
@@ -68,7 +73,7 @@ par(mai=c(.8,.8,.1,.1), mgp=c(2.7,1,0))
 
 # red
 startp <- 0
-endp <- 500
+endp <- 100
 lcolor <- colorRampPalette(c("black","red"))(endp)
 plot(as.numeric(dat2[1,7:42]) ~ I(seq(390, 740, 10)), type="l",
     col=lcolor[startp], ylim=c(0,2.2), xlab=expression(paste(lambda, " in nm")), ylab="Intensity")
@@ -77,22 +82,21 @@ for (i in startp:endp){
 }
 
 # green
-startp <- 501
-endp <- 1000
-lcolor <- colorRampPalette(c("black","green"))(500)
+startp <- 101
+endp <- 200
+lcolor <- colorRampPalette(c("black","green"))(100)
 lines(as.numeric(dat2[501,7:42]) ~ I(seq(390, 740, 10)), col=lcolor[1])
 for (i in startp:endp){
-    lines(as.numeric(dat2[i,7:42]) ~ I(seq(390, 740, 10)), col=lcolor[i-500])
+    lines(as.numeric(dat2[i,7:42]) ~ I(seq(390, 740, 10)), col=lcolor[i-100])
 }
 
 # blue
-startp <- 1001
-endp <- 1500
-lcolor <- colorRampPalette(c("black","blue"))(500)
+startp <- 201
+endp <- 300
+lcolor <- colorRampPalette(c("black","blue"))(100)
 lines(as.numeric(dat2[1001,7:42]) ~ I(seq(390, 740, 10)), col=lcolor[1]) 
 for (i in startp:endp){
-    lines(as.numeric(dat2[i,7:42]) ~ I(seq(390, 740, 10)),
-    col=lcolor[i-1000])
+    lines(as.numeric(dat2[i,7:42]) ~ I(seq(390, 740, 10)), col=lcolor[i-200])
 }
 
 dev.off()
@@ -100,23 +104,18 @@ dev.off()
 
 ###### (2) luminance curves ######
 
-dat3 <- read.table("achrolabutils/calibdata/measurements/calibration_tubes_raw_20120208_1833.txt", sep=",", skip=1)
-
-names(dat3) <- c("vR","vG","vB","x","y","Y",paste("l", 1:36, sep=""))
-
-
 ## fitted with nls()
-plot(Y ~ vR, dat3[1:500,], ylim=c(0, 40), pch=16, col="red",
-    xlab="voltage", ylab="luminance")
-points(Y ~ vG, dat3[501:1000,], col="green", pch=16)
-points(Y ~ vB, dat3[1001:1500,], col="blue", pch=16)
+plot(Y ~ vR, dat2[1:100,], ylim=c(0, 70), pch=16, col="red",
+    xlab="voltage", ylab="luminance", type="l", lwd=2)
+lines(Y ~ vG, dat2[101:200,], col="green", pch=16, lwd=2)
+lines(Y ~ vB, dat2[201:300,], col="blue", pch=16, lwd=2)
 
 # fit curves
-nlsR <- nls(Y ~ p1 + (p2 - p1)*exp(-exp(p3)*vR), data=dat3[1:500,],
+nlsR <- nls(Y ~ p1 + (p2 - p1)*exp(-exp(p3)*vR), data=dat2[1:100,],
         start=c(p1=50, p2=-10, p3=-7))
-nlsG <- nls(Y ~ p1 + (p2 - p1)*exp(-exp(p3)*vG), data=dat3[501:1000,],
+nlsG <- nls(Y ~ p1 + (p2 - p1)*exp(-exp(p3)*vG), data=dat2[101:200,],
         start=c(p1=50, p2=-10, p3=-7))
-nlsB <- nls(Y ~ p1 + (p2 - p1)*exp(-exp(p3)*vB), data=dat3[1001:1500,],
+nlsB <- nls(Y ~ p1 + (p2 - p1)*exp(-exp(p3)*vB), data=dat2[201:300,],
         start=c(p1=50, p2=-15, p3=-10))
 
 # predicted values
@@ -124,24 +123,27 @@ preR <- predict(nlsR)
 preG <- predict(nlsG)
 preB <- predict(nlsB)
 
-lines(preR ~ vR, dat3[1:500,])
-lines(preG ~ vR, dat3[1:500,])
-lines(preB ~ vR, dat3[1:500,])
+lines(preR ~ vR, dat2[1:100,])
+lines(preG ~ vR, dat2[1:100,])
+lines(preB ~ vR, dat2[1:100,])
 
-## fitted with lm() for medium values
-plot(Y ~ vR, dat3[100:400,], ylim=c(0, 40), pch=16, col="red",
-    xlab="voltage", ylab="luminance")
-points(Y ~ vG, dat3[600:900,], col="green", pch=16)
-points(Y ~ vB, dat3[1100:1400,], col="blue", pch=16)
+## all three tubes together
+dat3 <- read.table("achrolabutils/calibdata/measurements/calibration_tubes_raw20120210_1050.txt", sep=",", skip=1)
+names(dat3) <- c("vR","vG","vB","x","y","Y",paste("l", 1:36, sep=""))
 
-lmR <- lm(Y ~ vR, dat3[100:400,])
-lmG <- lm(Y ~ vG, dat3[600:900,])
-lmB <- lm(Y ~ vB, dat3[1100:1400,])
+# five measurements per step
+dat3 <- aggregate(as.matrix(dat3[,4:ncol(dat3)]) ~ vR + vG + vB, dat3, mean)
 
-abline(lmR)
-abline(lmG)
-abline(lmB)
-# Hmm, don't know. Looks good for blue but that also looks good with
-# nls()...
+dat3 <- dat3[order(dat3$vR, dat3$vG, dat3$vB),]
+names(dat3) <- c("vR","vG","vB","x","y","Y",paste("l", 1:36, sep=""))
 
+lines(Y ~ vB, dat3, pch=16, lwd=2)
+
+# are 3 channels additive?
+Yadd <- dat2[1:100,6] + dat2[101:200,6] + dat2[201:300,6]
+
+lines(Yadd ~ vR, dat3, pch=16, col="grey", lwd=2)
+
+legend(1000, 70, c("measured","sum"), col=c("black","grey"), lty=1, lwd=2,
+    bty="n")
 
