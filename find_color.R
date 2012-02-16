8924bf8382e82bf2fa9c64913d6de66818033634
Nora Umbach <nora.umbach@web.de>
2012-02-16 18:47:10 +0100

    cleaned up files, added plots

--- find_color.R
+++ find_color.R
@@ -2,6 +2,18 @@
 #
 # Find voltages that adjust tubes so that xyY for tubes has a minimum
 # distance from monitor xyY.
+#
+# content: (1) read data for tubes and monitor
+#          (2) find best fit
+#          (3) diagnostic plots
+#
+# input: measure_tubes_20120210_2151.txt
+#        depth_monitor20120210_1949.txt
+# output: --
+#
+# last mod: Feb/16/2012, NU
+
+###### (1) read data for tubes and monitor ######
 
 tub0 <- read.table("measure_tubes_20120210_2151.txt", header=T, sep=",")
 # Careful! This thing is huge!
@@ -15,6 +27,8 @@ names(mon0) <- c("R","G","B","x","y","Y",paste("l", 1:36, sep=""))
 mon <- aggregate(as.matrix(mon0) ~ R + G + B, mon0, mean)
 for (i in 1:3) mon[,4] <- NULL
 names(mon) <- c("R","G","B","x","y","Y",paste("l", 1:36, sep=""))
+
+###### (2) find best fit ######
 
 res_x <- NULL
 for (i in 1:10){
@@ -38,7 +52,7 @@ for (i in 1:10){
 }
 
 
-
+## first for x and y coosrdinates
 
 for (i in 1:10){
     tub[paste("r", i, sep="")] <- sqrt((tub$x - mon$x[i])^2  + (tub$y -
@@ -47,6 +61,8 @@ for (i in 1:10){
 
 epsilon <- 0.0035
 
+## then for Y
+
 reslist <- NULL
 for (i in 1:10){
     tmp <- tub[tub[paste("r", i, sep="")] < epsilon,]
@@ -54,6 +70,9 @@ for (i in 1:10){
     reslist <- rbind(reslist, tmp[which.min(abs(tmp$Y - mon$Y[i])),])
 }
 
+###### (3) diagnostic plots ######
+
+# Did we measure the correct area?
 par(mfrow=c(1,2))
 # diagnostic plot xy
 plot(tub$x, tub$y, pch=".", xlim=c(0.296, 0.306))
@@ -62,6 +81,7 @@ points(mon$x, mon$y, pch="x", col="red")
 plot(jitter(rep(1, length(tub$Y))), tub$Y, pch=".", ylim=c(19,21.5))
 abline(h=mon$Y, col="red")
 
+# How does the color influence direction?
 par(mfrow=c(1,3))
 # red
 plot(tub$x, tub$y, type="n", xlim=c(0.296, 0.306))
@@ -88,17 +108,20 @@ for (i in 1:length(unique(tub$volB))) {
     points(tmp$x, tmp$y, col=lcolor[i], pch=".")
     }
 
+# Look at deviance of our measurements:
+# ellipse function
 ell <- function(t, xc=0, yc=0, a=1, b=1/5, phi=0){
   phi <- ifelse(phi < pi/2, phi, pi - phi)
   cbind(x = xc + a*cos(t)*cos(phi) - b*sin(t)*sin(phi),
         y = yc + a*cos(t)*sin(phi) + b*sin(t)*cos(phi))
 }
 
+steps <- 1
 tub.tmp <- tub
-tub <- tub[tub$volR==1275 & tub$volG==1682,]
+tub <- tub[tub$volR==1270 & tub$volG==1670,]
 tub <- tub[order(tub$volB),]
 print(dim(tub))
-idx <- seq(1,46,8)
+idx <- seq(1,46,steps)
 plot(tub$x, tub$y, type="n")
 lcolor <- colorRampPalette(c("green","red"))(length(idx))
 for (j in 1:length(idx)) {
@@ -114,8 +137,7 @@ for (j in 1:length(idx)) {
     lines(ell(seq(0,2*pi,length.out=100), tub$x[i], tub$y[i], xsd, ysd),
     col=lcolor[j])
     }
+lines(tub$x, tub$y)
 tub <- tub.tmp
 rm(tub.tmp)
-    
 
-    
