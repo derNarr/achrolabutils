7f2c397339c02f417c1b0002c6a324ed8ff4d7cc
Nora Umbach <nora.umbach@web.de>
2012-05-22 17:34:20 +0200

    merge requested commit

--- monitorDepth.py
+++ monitorDepth.py
@@ -148,8 +148,8 @@ if(__name__=="__main__"):
 
     ## all greys on EIZO GS320
     import eizoGS320
-    #patch_stim_rgb = [eizoGS320.encode_color(x, x) for x in range(697,700)]
-    patch_stim_rgb = [eizoGS320.encode_color(866, 871),]
+    patch_stim_rgb = [eizoGS320.encode_color(x, x) for x in range(0,1024)]
+    #patch_stim_rgb = [eizoGS320.encode_color(871, 871),]
     print(patch_stim_rgb)
 
     mywin = visual.Window(size=(1024,1536), monitor='mymon',
