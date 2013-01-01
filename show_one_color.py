0183e6eefd1d04c54a60e02e2abe2904e84026d2
Konstantin Sering <konstantin.sering@gmail.com>
2013-01-01 20:27:13 +0100

    [py/BF] updated show_one_color.py

--- show_one_color.py
+++ show_one_color.py
@@ -13,15 +13,15 @@
 # output: --
 #
 # created
-# last mod 2011-05-01, DW
+# last mod 2013-01-01 12:37 KS
 
-from psychopy import visual, event, core
+from psychopy import visual, core, event
 
 from achrolab.devtubes import DevTubes
 from achrolab.monitor import Monitor
 from achrolab.eyeone.eyeone import EyeOne
 from numpy import repeat
-from monitor import eizoGS320
+from stimuli import eizoGS320
 import Image
 
 tub = DevTubes()
@@ -34,15 +34,15 @@ mon = Monitor(mywin)
 # bg = 0.301960784314
 # voltages = (1592, 2316, 2234)
 # mon.setPatchStimColor(bg)
-# 
+#
 # wasco.wasco_outportW(boardId, DAOUT3_16, voltages[0])
 # wasco.wasco_outportW(boardId, DAOUT1_16, voltages[1])
 # wasco.wasco_outportW(boardId, DAOUT2_16, voltages[2])
- 
+
 ## create bitmaps
 
 print("Create Images\nThis can take some time...\n")
-bg = 600
+color = bg = 600
 
 # background that just fills whole monitor with a certain color
 a_bg = repeat(bg, 2048*1536).reshape(1536, 2048)
@@ -66,21 +66,22 @@ tub.setVoltages(voltages)
 # show bitmaps
 # bg = visual.SimpleImageStim(mywin, "background" + str(id) + ".bmp",
 # units="pix")
+#print("load image to psychopy\n")
+#bg = visual.SimpleImageStim(mywin, "same.bmp", units="pix")
+#print("done.\n\nDraw now!\n")
 
-print("load image to psychopy\n")
-bg = visual.SimpleImageStim(mywin, "same.bmp", units="pix")
-print("done.\n\nDraw now!\n")
+bg = visual.GratingStim(mywin, tex=None, mask=None, units="norm", pos=(0, 0),
+                        size=(2, 2), color=eizoGS320.encode_color(color),
+                        colorSpace="rgb255")
 bg.draw()
 mywin.flip()
 
-core.wait(60)
-
-# mouse = event.Mouse(mywin)
-# show = True 
-# while show:
-#     core.wait(0.01)
-#     left, middle, right = mouse.getPressed()
-#     if left: 
-#         core.wait(0.2)
-#         show=False
+mouse = event.Mouse(mywin)
+show = True
+while show:
+    core.wait(0.01)
+    left, middle, right = mouse.getPressed()
+    if left:
+        core.wait(0.2)
+        show=False
 
